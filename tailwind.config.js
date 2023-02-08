033482250d866a5932435c1d2897bda374cc940a
nico1988 <410827024@qq.com>
2023-02-08 22:44:22 +0800

    feat: add prettierignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '500px',
+        md: '800px',
+        lg: '1200px',
+        xl: '1536px',
+      },
+      colors: {
+        brand: {
+          accent: '#5AD769',
+          main: '#324BC3',
+        },
+        text: {
+          primary: '#FFFFFF',
+          secondary: '#0C0F19',
+        },
+        background: {
+          primary: '#0C0F19',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+        mono: [...defaultTheme.fontFamily.mono],
+        poppins: ['var(--font-poppins)'],
+      },
+    },
   },
   plugins: [],
 };

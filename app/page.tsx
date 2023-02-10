4edc326629af7ad9b5f36ea76af0ad34e742dc98
nico1988 <410827024@qq.com>
2023-02-10 16:13:25 +0800

    feat: add auto resort class

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes';
 const Index = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <button className="px-5 py-2 rounded-xl bg-orange-200 text-orange-500 text-lg font-medium hover:bg-orange-300 active:bg-orange-200 active:scale-95 duration-300">
+      <button className="rounded-xl px-5 py-2 bg-orange-200 text-orange-500 text-lg font-medium hover:bg-orange-300 active:bg-orange-200 active:scale-95 duration-300">
         Hello, Next.js 13!
       </button>
 

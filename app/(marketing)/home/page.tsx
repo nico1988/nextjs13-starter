abe10ded5d5361c0f6c8d16bdaace34d5f55a131
nico1988 <410827024@qq.com>
2023-02-12 02:26:53 +0800

    feat: add route grouping

--- app/(marketing)/home/page.tsx
+++ app/(marketing)/home/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen items-center justify-center">
       <p>This is the marketing home page.</p>
       <p>But you don not need to type marketing in the route</p>
     </div>

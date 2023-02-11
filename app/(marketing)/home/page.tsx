d9c52d16a2aba82b338e3698109f0d556cca7511
nico1988 <410827024@qq.com>
2023-02-12 02:26:27 +0800

    feat: add route grouping

--- app/(marketing)/home/page.tsx
+++ app/(marketing)/home/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <p>This is the marketing page.</p>
+      <p>This is the marketing home page.</p>
+      <p>But you don not need to type marketing in the route</p>
     </div>
   );
 }

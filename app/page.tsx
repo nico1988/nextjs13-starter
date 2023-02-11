c43dede0f78a1e1bc1ab8b358b783f5e8e9fbfb1
nico1988 <410827024@qq.com>
2023-02-12 00:56:42 +0800

    fix: react/button-has-type :

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <button className="rounded-xl bg-orange-200 px-5 py-2 text-lg font-medium text-orange-500 duration-300 hover:bg-orange-300 active:scale-95 active:bg-orange-200">
+      <button className="rounded-xl bg-orange-200 px-5 py-2  text-lg font-medium text-orange-500 duration-300 hover:bg-orange-300 active:scale-95 active:bg-orange-200">
         Hello, Next.js 13!
       </button>
     </div>

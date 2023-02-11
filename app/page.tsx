a7c49464d5ac5626bf042aeaa7cfa618dc608a64
nico1988 <410827024@qq.com>
2023-02-11 13:30:31 +0800

    feat: change prettier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { ThemeProvider } from 'next-themes';
 
-const Index = () => {
+function Index() {
   return (
     <div className="flex items-center justify-center h-screen">
       <button className="rounded-xl px-5 py-2 bg-orange-200 text-orange-500 text-lg font-medium hover:bg-orange-300 active:bg-orange-200 active:scale-95 duration-300">
         Hello, Next.js 13!
       </button>
 
-      <div className=''>
+      <div className="text-6xl text-[#fb0067]">
+        222
       </div>
     </div>
   );
-};
+}
 
 export default Index;

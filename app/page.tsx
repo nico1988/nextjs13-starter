c044790227718f7deeb2d5493cd7094db0d054cc
nico1988 <410827024@qq.com>
2023-05-14 08:44:43 +0800

    feat: add milky-green-v1.avif

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,14 @@ export default function Index() {
         <h1 className="mb-3 cursor-pointer text-center text-4xl">LAPLACE</h1>
         <div className="cursor-pointer text-center">奶绿只是想偷偷睡一会</div>
       </div>
+      <div className="absolute bottom-0 flex w-full justify-center">
+        <Image
+          className="left-0 right-0 m-auto"
+          src="/milky-green-v1.avif"
+          width={800}
+          height={800}
+        />
+      </div>
     </main>
   );
 }
